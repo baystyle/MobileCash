2f068bb0284104cf07cb97b94fa458ef3bfb0535
Theo Seathan <60160109@go.buu.ac.th>
2020-03-02 20:10:44 +0700

    view transaction

--- MobileCash/src/app/tab1/tab1.page.ts
+++ MobileCash/src/app/tab1/tab1.page.ts
@@ -10,7 +10,42 @@ import { Observable } from 'rxjs';
 })
 export class Tab1Page {
 
+  private dateCurrent;
+  private balance;
+  private account;
+  private startDate = this.datePipe.transform(new Date(),'dd/MM/yyyy');
+  private endDate = this.datePipe.transform(new Date(),'dd/MM/yyyy');
+  private totalCost;
+
   constructor(private datePipe:DatePipe,private http:HttpClient) {
+
+    this.dateCurrent = this.datePipe.transform(new Date(),'dd/MM/yyyy');
+    this.getAccount();
   
+  }
+
+  getAccount(){
+    let url = 'http://localhost/MCash/Account/getAccount.php';
+    let data:Observable<any> = this.http.post(url,'');
+    data.subscribe(data => {
+      this.balance = data[0].acc_balance
+      this.account = data[0].acc_id
+      console.log(this.balance);
+      console.log(this.account);
+    })    
+  }
+
+  calculate(){
+    let url = 'http://localhost/Mcash/Transaction/calculateDate.php';
+    let dataPost = new FormData();
+    dataPost.append("start_date",this.startDate);
+    dataPost.append("end_date",this.endDate);
+
+    let data:Observable<any> = this.http.post(url,dataPost);
+    data.subscribe(data => {
+      console.log("success");
+      this.totalCost = data[0].sum_amount;
+      console.log("sum = ",this.totalCost);
+    }) 
   }
 }
